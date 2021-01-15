c39468ac2c36347a7e9375a42f865b0eb01fde1c
thantmyat <thantmyat1234@gmail.com>
2021-01-15 16:49:45 +0630

    added table page and /table route

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,19 +3,22 @@ import React from 'react';
 import DBLayout from './../../components/DBLayout/DBLayout';
 import Details from './Details/Details';
 
-import { BsFillGridFill } from 'react-icons/bs';
+import { BsFillGridFill, BsTable } from 'react-icons/bs';
 import { FaWpforms } from 'react-icons/fa';
 import Form from './Form/Form';
+import Table from './Table/Table';
  
 const Dashboard = () => {
     const navLinks = [
         { path: '/', name: 'Dashboard', icon: <BsFillGridFill /> },
-        { path: '/form', name: 'Form', icon: <FaWpforms /> }
+        { path: '/form', name: 'Form', icon: <FaWpforms /> },
+        { path: '/table', name: 'Table', icon: <BsTable /> }
     ];
 
     const routes = [
         { path: '/', component: Details },
-        { path: '/form', component: Form }
+        { path: '/form', component: Form },
+        { path: '/table', component: Table }
     ];
 
     return (
---
+++ src/pages/Dashboard/Table/Table.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from './../../../components/Title/Title';
+
+const Table = () => {
+    return (
+        <>
+            <Title title="table" />
+        </>
+    );
+}
+ 
+export default Table;
