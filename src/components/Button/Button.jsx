d2a99d2d7e80f9ec5483cafd086bab10fc07520a
thantmyat <thantmyat1234@gmail.com>
2021-01-25 13:43:42 +0630

    added popup component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './Button.module.css';
+import cx from 'classnames';
 
-const Button = ({ title, type, style }) => {
+const Button = ({ title, type, style, onClick, btnColor }) => {
     return (
         <button 
             type={type} 
             style={style}
-            className={styles.button}>{title}</button>
+            onClick={onClick}
+            className={
+                btnColor && btnColor === 'danger'?
+                cx(styles.button, styles.danger):
+                styles.button
+            }>{title}</button>
     );
 }
  
--- src/components/CardTable/CardTable.jsx
+++ src/components/CardTable/CardTable.jsx
@@ -1,40 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Popup from './../Popup/Popup';
 import Button from '../Button/Button';
 import styles from './CardTable.module.css';
 
 const CardTable = () => {
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    const handleOnClick = () => {
+        setIsPopupOpen(true);
+    }
+
+    const handleOnDelete = () => {
+        console.log("delete");
+        setIsPopupOpen(false);
+    }
+
     return (
-        <table className={styles.table}>
-            <thead>
-                <tr>
-                    <th>No.</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    <th>Options</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>John Carter</td>
-                    <td>johncarter@email.com</td>
-                    <td>Subscriber</td>
-                    <td>
-                        <Button title="Change Role" style={{ padding:'5px', backgroundColor:'#D8CBD6' }} />
-                    </td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>John Carter</td>
-                    <td>johncarter@email.com</td>
-                    <td>Subscriber</td>
-                    <td>
-                        <Button title="Change Role" style={{ padding:'5px', backgroundColor:'#D8CBD6' }} />
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Options</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>John Carter</td>
+                        <td>johncarter@email.com</td>
+                        <td>Subscriber</td>
+                        <td>
+                            <Button title="Change Role" style={{ padding:'5px', backgroundColor:'#D8CBD6' }} onClick={handleOnClick} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>John Carter</td>
+                        <td>johncarter@email.com</td>
+                        <td>Subscriber</td>
+                        <td>
+                            <Button title="Change Role" style={{ padding:'5px', backgroundColor:'#D8CBD6' }} onClick={handleOnClick} />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+            {isPopupOpen ?
+                <Popup title="Are you sure to delete this user?">
+                    <div>
+                        <Button 
+                            title="cancel" 
+                            btnColor="danger"
+                            onClick={() => setIsPopupOpen(false)} 
+                        />
+                        <Button 
+                            title="delete" 
+                            onClick={handleOnDelete}
+                            style={{ marginLeft:'10px' }}
+                        />
+                    </div>
+                </Popup> :
+                null
+            }
+        </>
     );
 }
  
